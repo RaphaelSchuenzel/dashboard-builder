2ee9473b80d3008a5360302b58f6089d2a4bd855
Raphael Schünzel <inbox+git@raphaelschuenzel.com>
2022-11-10 21:45:22 +0100

    chart components

--- app.vue
+++ app.vue
@@ -1,12 +1,12 @@
 <template lang="pug">
-#dashboard.h-screen.w-screen.bg-gray-200
+#dashboard.bg-gray-200
     .bg-gray-50
         .flex.items-center.justify-between.mx-6.px-6.py-4
             p ACME
 
             a.border-2.p-4 Add Component
 
-    .mx-6.mt-8
+    .mx-6.py-8
         #components.grid.grid-cols-4
             Component(
                 v-for="component in components"
@@ -18,17 +18,17 @@
 const components = [
     {
         name: 'Monthly Profit',
-        chart: 'line',
-        data: [{x: 10, y: 20}, {x: 15, y: null}, {x: 20, y: 10}],
-    },
-    {
-        name: 'Average Price',
         chart: 'bar',
         data: [{x: 10, y: 20}, {x: 15, y: null}, {x: 20, y: 10}],
     },
     {
+        name: 'Average Price',
+        chart: 'pie',
+        data: [{x: 10, y: 20}, {x: 15, y: null}, {x: 20, y: 10}],
+    },
+    {
         name: 'Sales per Month',
-        chart: 'line',
+        chart: 'bar',
         data: [{x: 10, y: 20}, {x: 15, y: null}, {x: 20, y: 10}],
     },
     {
--- components/Component.vue
+++ components/Component.vue
@@ -2,12 +2,16 @@
 .component.bg-gray-50.mx-6.my-4.p-4
     h1 {{ $attrs.component.name }}
 
-    Bar(
-        :chart-data="$attrs.component.data"
+    BarChart(
         v-if="$attrs.component.chart === 'bar'"
+    )
+
+    PieChart(
+        v-if="$attrs.component.chart === 'pie'"
     )
 </template>
 
 <script setup>
-import { Bar } from 'vue-chartjs'
+import BarChart from './charts/barChart'
+import PieChart from './charts/pieChart'
 </script>
